9bcf2cc8e31ceb72d7027198facb248da074993b
abdulloh070 <abdulloh.tolipov99@gmail.com>
2023-04-30 02:42:14 +0500

    second changes

--- build/src/constants/regex.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.emailRegex = exports.objectIdRegex = void 0;
-exports.objectIdRegex = /^[0-9a-fA-F]{24}$/;
-exports.emailRegex = /^(?!.*@[^,]*,)/;
--- build/src/utils/changeResponse.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.changeResponse = void 0;
-const changeResponse = (success, data, error = null) => ({
-    success,
-    data,
-    error,
-});
-exports.changeResponse = changeResponse;
